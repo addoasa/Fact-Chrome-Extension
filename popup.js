3c3cf86535c96df5a31e6043bf7efc526eb7dbb1
Kwadwo <addoasa94@gmail.com>
2019-06-13 09:54:46 -0400

    Added chuck joke generator, and semi functional clipboard

--- popup.js
+++ popup.js
@@ -1,31 +1,106 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-'use strict';
 
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+let dataObj = {};
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+const clipboard = []
+const clipboardArea = document.querySelector('#theClipboard')
+let clipboardIncrementer = 0
+const mainBody = document.querySelector('#mainContent')
+document.body.appendChild(mainBody)
+// ------------------------------------------------------------------------------------
+
+const jokeContainer = document.createElement('div');
+jokeContainer.classList.add('joke-container')
+mainBody.appendChild(jokeContainer)
+const norrisPic = document.querySelector('.norrislogo')
+
+const jokeButton = document.querySelector('#jokeButton');
+norrisPic.addEventListener('click', getJoke );
+// ------------------------------------------------------------------------------------
+
+
+let url = 'https://api.chucknorris.io/jokes/random?category=dev'
+
+function getJoke(){
+
+  fetch(url) 
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    const newJoke = document.createElement('h3')
+    newJoke.classList.add('joke')
+    newJoke.innerText = JSON.stringify(myJson.value)
+    jokeContainer.appendChild(newJoke)
+    console.log(JSON.stringify(myJson.value));
+    responsiveVoice.speak(JSON.stringify(myJson.value))
   });
-};
 
-let unirest = require('unirest');
+}
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// setInterval(getContent,1000 )
+
+// function getContent(){
+//     chrome.storage.local.get(['content'], function(result) {
+//     if(result.content !== undefined){
+//       console.log('Value currently is ' + result.content)
+//       //  newClipboardEntry.innerText = result.content
+//     // clipboard.push(result.content)
+//       console.log(clipboard)
+//   // clipboardArea.appendChild(newClipboardEntry)
+
+//     }
+   
+//   });
+// }
+
+function copyText(){
+  let copiedText = document.getElementById("txt_copy")
+  copiedText.select()
+  document.execCommand('copy');
+
+  // if(clipboard.length !== 0){
+      // for(let i = clipboardIncrementer; i < clipboard.length; i++){
+        let newClipboardEntry = document.createElement('h3')
+        newClipboardEntry.classList.add('clipitem')
+        let content = copiedText.value
+        // ------------------------
+        dataObj[content] = content;
+
+        chrome.storage.local.set(dataObj, function() {          
+          //  alert('Settings saved');
+           console.log(dataObj)
+
+        });
+        // chrome.storage.local.set({[content]: copiedText.value}, function() {
+        //   // Notify that we saved.
+        //   alert('Settings saved');
+        // });
+
+        chrome.storage.sync.get(['content'], function(result) {
+            console.log('Value currently is ' + result.content)
+            newClipboardEntry.innerText = content
+            // clipboard.push(result.content)
+
+        clipboardArea.appendChild(newClipboardEntry)
+
+          });
+      //   chrome.storage.local.get(['key'], function(result) {
+      //   console.log('Value currently is ' + result.key);
+      // });
+       
 
 
-unirest.get("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random")
-.header("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com")
-.header("X-RapidAPI-Key", "e2d7137386mshc3d621b0db1afefp1aa8bajsna8975bcc80d1")
-.header("accept", "application/json")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
+        // clipboardIncrementer++ 
+      // }
+  // }
+  // alert("Copied the text: " + copiedText.value);
+
+ }
+// ------------------------------------------------------------------------------------
+ const copyButton = document.querySelector('#copy')
+ copyButton.addEventListener('click', copyText)
+
+
+
